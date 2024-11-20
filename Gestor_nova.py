1c4d6936addbd07e79b77bc5ebd23a988f332416
vctr23 <vsanchezmel23@gmail.com>
2024-11-20 02:57:22 +0100

    Initial commit

--- Gestor_nova.py
+++ Gestor_nova.py
@@ -11,6 +11,7 @@ import tkinter as tk
 from tkinter import ttk
 import customtkinter as ctk
 import Main
+import datetime
 #%% 3. Code
 def center_window(root, window_width):
     screen_width = root.winfo_screenwidth()
@@ -30,7 +31,7 @@ class MainWindow():
     Return -> None.
     """
     def __init__(self, root):
-        ctk.set_appearance_mode("system")
+        ctk.set_appearance_mode("dark")
         self.root = root
         self.root.title("NovaTM")
         center_window(self.root, 1100)
@@ -46,7 +47,7 @@ class MainWindow():
         self.frame_top = FrameTop(self.root, self.frame_mid.task_treeview)
         self.frame_top.grid(row = 0, column = 0, sticky = tk.NSEW, padx = 20, pady = 10)
                 
-        self.frame_bot = FrameBot(self.root)
+        self.frame_bot = FrameBot(self.root, self.frame_mid.task_treeview)
         self.frame_bot.grid(row = 2, column = 0, sticky = tk.NSEW, padx = 20, pady = 10)
         
         
@@ -63,12 +64,15 @@ class FrameTop(ctk.CTkFrame):
         self.list = ctk.CTkComboBox(self, values = ["Alta", "Media", "Baja"], font = ("Helvetica",16))
         self.list.grid(row = 0, column = 1 , sticky = tk.EW, padx = 30, pady = 20)
         
-        self.add_button = ctk.CTkButton(self, text = "Add task", command = lambda: Main.add_task(self.task_entry, self.list, task_treeview),
+        self.add_button = ctk.CTkButton(self, text = "Add task", 
+                                        command = lambda: Main.add_task(self.task_entry, self.list, task_treeview, self.task_label),
                                         corner_radius= 20, border_width = 2, 
                                         fg_color="transparent", font = ("Helvetica",16), hover_color="#3A3434")
         self.add_button.grid(row = 0, column = 2, sticky = tk.EW, padx = 20, pady = 20)
         
-        
+        self.task_label = ctk.CTkLabel(self, text = "Tareas:", font = ("Helvetica", 16))
+        self.task_label.grid(row = 1, column = 1, sticky = tk.EW, pady = 10)
+
 class FrameMid(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
@@ -82,22 +86,36 @@ class FrameMid(ctk.CTkFrame):
         self.task_treeview.heading("Estado", text = "Estado")
         self.task_treeview.heading("Fecha", text = "Fecha")
         self.task_treeview.grid(row = 0, column = 0, sticky = tk.NSEW)
+        
+        # See if the scrollbar works:
+        # contacts = []
+        # for n in range(1, 100):
+        #     contacts.append((f'first {n}', f'Alta {n}', f'Activo', datetime.datetime.now()))
+
+        # for contact in contacts:
+        #     self.task_treeview.insert('', tk.END, values=contact)
 
         self.scroll = ttk.Scrollbar(self, orient="vertical", command = self.task_treeview.yview)
         self.task_treeview.configure(yscroll = self.scroll.set)
         self.scroll.grid(row = 0, column = 1, sticky = tk.NS)
         
 class FrameBot(ctk.CTkFrame):
-    def __init__(self, master):
+    def __init__(self, master, task_treeview):
         super().__init__(master)
 
         self.grid_rowconfigure(0, weight = 1)
         self.grid_columnconfigure([0, 1, 2], weight = 1)
 
-        self.button_eliminate = ctk.CTkButton(self, text = "Eliminar", fg_color="transparent", border_width = 2, corner_radius = 20)
+        self.button_eliminate = ctk.CTkButton(self, text = "Eliminar", command = lambda: Main.eliminate(task_treeview), 
+                                            fg_color="transparent", border_width = 2, corner_radius = 20)
         self.button_eliminate.grid(row = 0, column = 0, sticky = tk.NSEW, padx = 10)
 
-        self.light_dark_button = ctk.CTkButton(self, text = "a", fg_color="transparent", border_width = 2, corner_radius = 20)
+        self.button_completed = ctk.CTkButton(self, text="Mark as completed", 
+                                            fg_color="transparent", border_width = 2, corner_radius = 20)
+        self.button_completed.grid(row = 0, column = 1, sticky = tk.NSEW, padx = 10)
+
+        self.light_dark_button = ctk.CTkButton(self, text = "a", 
+                                            fg_color="transparent", border_width = 2, corner_radius = 20)
         self.light_dark_button.grid(row = 0, column = 2, sticky = tk.NSEW, padx = 10)
 
         
--- Main.py
+++ Main.py
@@ -8,23 +8,42 @@ Created on Tue Nov 19 16:14:03 2024
 
 #%% 2. Imports
 import tkinter as tk
+from tkinter import messagebox
 import customtkinter as ctk
 import datetime
 import Gestor_nova
 
 #%% 3. Code
-def add_task(entry, list, task_treeview):
+counter = 0
+def add_task(entry, list, task_treeview, label_task):
+    global counter
     task = entry.get()
     priority = list.get()
     state = "Activo"
     date = datetime.datetime.now()
 
-    insert = task_treeview.insert('', tk.END, values = [task, state, priority, date])
+    if task.isalnum() and len(task) != 0:
+        counter = counter + 1
+        entry.delete(0, "end")
+        insert = task_treeview.insert('', tk.END, values = [task, priority, state, date])
+        label_task.configure(text = f"Tareas: {counter}")
+        return insert
+    else:
+        # Show error message when entry is empty
+        messagebox.showerror(title = "ERROR",message = "Error al introducir. El entry está vacío")
+        return
+  
+def eliminate(task_treeview):
+    obj = task_treeview.selection()[0]
+    task_treeview.delete(obj)
+    if not task_treeview.exists(obj):
+        messagebox.showinfo(title = "Deleted", message = "Fila eliminada correctamente")
+    else:
+        return
 
-    return insert
 
-
-
+    
+    
 
 
 #%% 4. Main
