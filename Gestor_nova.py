6ceb0832b184c3d6d4f960e8ad0825f58fc166a1
vctr23 <vsanchezmel23@gmail.com>
2024-11-20 22:58:43 +0100

    Ninth commit

--- Gestor_nova.py
+++ Gestor_nova.py
@@ -39,9 +39,11 @@ class MainWindow():
         self.root.grid_rowconfigure(0, weight = 1)
         self.root.grid_rowconfigure(1, weight = 10)
         self.root.grid_rowconfigure(2, weight = 1)
+        self.root.grid_rowconfigure(3, weight = 1)
         self.root.columnconfigure(0, weight = 1)
         style()
-              
+        notebook_style()
+        
         self.frame_mid = FrameMid(self.root)
         self.frame_mid.grid(row = 1, column = 0, sticky = tk.NSEW, padx = 20)
 
@@ -49,13 +51,13 @@ class MainWindow():
         self.frame_top.grid(row = 0, column = 0, sticky = tk.NSEW, padx = 20, pady = 10)
                 
         self.frame_bot = FrameBot(self.root, self.frame_mid.task_treeview, self.frame_top.task_label,
-                                self.frame_top.label_completed_task)
+                                self.frame_top.label_completed_task, self.frame_mid.notebook)
         self.frame_bot.grid(row = 2, column = 0, sticky = tk.NSEW, padx = 20, pady = 10)
+        
         
         self.upload = Main.import_from_json(self.frame_mid.task_treeview, self.frame_top.task_label,
                                             self.frame_top.label_completed_task)
     
-        
 class FrameTop(ctk.CTkFrame):
     def __init__(self, master, task_treeview):
         super().__init__(master)
@@ -86,31 +88,45 @@ class FrameMid(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
-        self.grid_rowconfigure(0, weight = 1)
-        self.grid_columnconfigure(0, weight = 1)
-        self.task_treeview = ttk.Treeview(self, columns = ["Task", "Priority", "State", "Date"],
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)       
+        
+        self.notebook = ttk.Notebook(self, style="Dark.TNotebook")
+        self.notebook.grid(row=0, column=0, sticky=tk.NSEW)
+        self.tab1 = ctk.CTkFrame(self.notebook)  
+        self.tab2 = ctk.CTkFrame(self.notebook)  
+        self.notebook.add(self.tab1, text="Task Manager")
+        self.notebook.add(self.tab2, text="Gantt chart")
+         
+        self.tab1.grid_rowconfigure(0, weight=1)
+        self.tab1.grid_columnconfigure(0, weight=1)
+         
+        self.task_treeview = ttk.Treeview(self.tab1, columns = ["Task", "Priority", "State", "Date", "EndDate"],
                                          show = "headings", style="Dark.Treeview")
         self.task_treeview.heading("Task", text = "Task")
         self.task_treeview.heading("Priority", text = "Priority")
         self.task_treeview.heading("State", text = "State")
         self.task_treeview.heading("Date", text = "Date")
+        self.task_treeview.heading("EndDate", text = "EndDate")
         self.task_treeview.grid(row = 0, column = 0, sticky = tk.NSEW)
         
         self.task_treeview.bind("<Double-1>", lambda event: Main.edit_tasks(event, self.task_treeview))     
-        # See if the scrollbar works:
-        # contacts = []
-        # for n in range(1, 100):
-        #     contacts.append((f'first {n}', f'Alta {n}', f'Activo', datetime.datetime.now()))
-
-        # for contact in contacts:
-        #     self.task_treeview.insert('', tk.END, values=contact)
-
-        self.scroll = ttk.Scrollbar(self, orient="vertical", command = self.task_treeview.yview)
+ 
+        self.scroll = ttk.Scrollbar(self.tab1, orient="vertical", command = self.task_treeview.yview)
         self.task_treeview.configure(yscroll = self.scroll.set)
         self.scroll.grid(row = 0, column = 1, sticky = tk.NS)
         
+        self.tab2.grid_columnconfigure(0, weight = 1)
+        self.tab2.grid_rowconfigure(0, weight = 1)
+        self.tab2.grid_rowconfigure(1, weight = 10)
+        
+        self.button_gantt = ctk.CTkButton(self.tab2, text = "Generate Gantt", 
+                         command = lambda: Main.generate_gantt(self.tab2, self.task_treeview))
+        self.button_gantt.grid(row = 0, column = 0, sticky = tk.N)
+        
+        
 class FrameBot(ctk.CTkFrame):
-    def __init__(self, master, task_treeview, label_task, label_completed_task):
+    def __init__(self, master, task_treeview, label_task, label_completed_task, notebook):
         super().__init__(master)
 
         self.grid_rowconfigure(0, weight = 1)
@@ -130,7 +146,7 @@ class FrameBot(ctk.CTkFrame):
         self.img_resized = self.img.resize((30, 30))
         self.img = ImageTk.PhotoImage(self.img_resized)
         self.light_dark_button = ctk.CTkButton(self, text = "", 
-                                            command = lambda: Main.change_appearance_mode(task_treeview),
+                                            command = lambda: Main.change_appearance_mode(task_treeview, notebook),
                                             image= ctk.CTkImage(dark_image=self.img_resized, light_image=self.img_resized),
                                             fg_color="transparent", border_width = 2, corner_radius = 20)
         self.light_dark_button.grid(row = 0, column = 2, sticky = tk.NSEW, padx = 10)
@@ -138,7 +154,7 @@ class FrameBot(ctk.CTkFrame):
 def style():
     # Dark mode style
     style = ttk.Style()
-    style.theme_use("clam")
+    style.theme_use("default")
     style.configure("Dark.Treeview",
                     background="#1e1e1e",  
                     foreground="white",  
@@ -167,3 +183,17 @@ def style():
     style.map("Light.Treeview.Heading",
               background=[("active", "#e0e0e0")],
               relief=[("pressed", "groove"), ("active", "raised")])
+    
+def notebook_style():
+    style = ttk.Style()
+    style.theme_use("default")  # Usa el tema por defecto para personalización completa
+    style.configure("Dark.TNotebook", background="black", borderwidth=0)
+    style.configure("Dark.TNotebook.Tab", background="gray20", foreground="white", padding=(10, 5))
+    style.map("Dark.TNotebook.Tab", background=[("selected", "black")], foreground=[("selected", "white")])
+    
+    style.configure("Light.TNotebook", background="white", borderwidth=0)
+    style.configure("Light.TNotebook.Tab", background="lightgray", foreground="black", padding=(10, 5))
+    style.map("Light.TNotebook.Tab", background=[("selected", "white")], foreground=[("selected", "black")])
+    
+    
+    
--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-#%% 1. Head
+# %% 1. Head
 # -*- coding: utf-8 -*-
 """
 Created on Tue Nov 19 16:14:03 2024
@@ -6,21 +6,26 @@ Created on Tue Nov 19 16:14:03 2024
 @author: vctr23
 """
 
-#%% 2. Imports
+# %% 2. Imports
+import tkinter as tk
 from tkinter import messagebox
 import customtkinter as ctk
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import pandas as pd
 import datetime
 import json
 import Gestor_nova
-
-#%% 3. Code
+# %% 3. Code
 def count_tasks(task_treeview, label_task, label_completed_task):
     number_tasks = len(task_treeview.get_children())
-    completed_task = sum(1 for task in task_treeview.get_children() 
-                        if task_treeview.item(task)["values"][2] == "Completed")
+    completed_task = sum(1 for task in task_treeview.get_children()
+                         if task_treeview.item(task)["values"][2] == "Completed")
 
-    label_task.configure(text = f"Total tasks: {number_tasks}")
-    label_completed_task.configure(text = f"Completed tasks: {completed_task}")
+    label_task.configure(text=f"Total tasks: {number_tasks}")
+    label_completed_task.configure(text=f"Completed tasks: {completed_task}")
+
 
 def add_task(entry, list, task_treeview, label_task, label_completed_task):
     task = entry.get()
@@ -28,75 +33,127 @@ def add_task(entry, list, task_treeview, label_task, label_completed_task):
     state = "Active"
     date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    if len(task) != 0: 
-        insert = task_treeview.insert("", "end", values = [task, priority, state, date])
+    if len(task) != 0:
+        insert = task_treeview.insert(
+            "", "end", values=[task, priority, state, date])
         count_tasks(task_treeview, label_task, label_completed_task)
         entry.delete(0, "end")
         return insert
     else:
-        messagebox.showerror(title = "ERROR",message = "Error could not insert. Entry is empty.")
+        messagebox.showerror(
+            title="ERROR", message="Error could not insert. Entry is empty.")
         return
-  
+
+
 def delete(task_treeview, label_task, label_completed_task):
     tasks = task_treeview.selection()
     if tasks:
         for task in tasks:
             task_treeview.delete(task)
         count_tasks(task_treeview, label_task, label_completed_task)
-        messagebox.showinfo(title = "Deleted", message = "Row/s deleted correctly.")
+        messagebox.showinfo(
+            title="Deleted", message="Row/s deleted correctly.")
     else:
-        messagebox.showwarning(title = "Warning", message = "Select at least one row to delete.")
+        messagebox.showwarning(
+            title="Warning", message="Select at least one row to delete.")
+
 
 def import_from_json(task_treeview, label_task, label_completed_task):
     with open("examples.json", "r", encoding="utf-8") as file:
         tasks = json.load(file)
         if tasks:
             for task in tasks:
-                task_treeview.insert("", "end", values = [task["Task"], task["Priority"], task["State"], task["Date"]])
+                task_treeview.insert("", "end", values=[
+                                     task["Task"], task["Priority"], task["State"], task["Date"], task["EndDate"]])
         count_tasks(task_treeview, label_task, label_completed_task)
-                
+
+
 def mark_as_completed(task_treeview, label_task, label_completed_task):
     tasks = task_treeview.selection()
     if tasks:
         for task in tasks:
             old_values = task_treeview.item(task, "values")
-            task_treeview.item(task, values = (old_values[0], old_values[1], "Completed", old_values[3]))
+            task_treeview.item(task, values=(
+                old_values[0], old_values[1], "Completed", old_values[3]))
         count_tasks(task_treeview, label_task, label_completed_task)
-        
 
     else:
-        messagebox.showwarning(title = "Warning", message = "Select at least one task to mark as completed.")
-        
+        messagebox.showwarning(
+            title="Warning", message="Select at least one task to mark as completed.")
+
+
 def edit_tasks(event, task_treeview):
     task = task_treeview.selection()
     if task:
-        ctkinput = ctk.CTkInputDialog(text = "Introduce the new task name:")
+        ctkinput = ctk.CTkInputDialog(text="Introduce the new task name:")
         task_name = ctkinput.get_input()
-        ctkinput2 = ctk.CTkInputDialog(text = "Introduce task priority (High, Medium, Low):")
+        ctkinput2 = ctk.CTkInputDialog(
+            text="Introduce task priority (High, Medium, Low):")
         priority = ctkinput2.get_input()
-        ctkinput3 = ctk.CTkInputDialog(text = "Introduce task state (Active, Completed):")
+        ctkinput3 = ctk.CTkInputDialog(
+            text="Introduce task state (Active, Completed):")
         state = ctkinput3.get_input()
         date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         if priority.strip() not in ["High", "Medium", "Low"]:
-            messagebox.showwarning(title = "Warning", message= "Write a correct priority value.")
+            messagebox.showwarning(
+                title="Warning", message="Write a correct priority value.")
             return
         elif state.strip() not in ["Active", "Completed"]:
-            messagebox.showwarning(title = "Warning", message= "Write a correct state value.")
+            messagebox.showwarning(
+                title="Warning", message="Write a correct state value.")
             return
         else:
-            task_treeview.item(task, values = (task_name, priority, state, date))
-            messagebox.showinfo(title = "Info", message = "Row editted succesfully")
+            task_treeview.item(task, values=(task_name, priority, state, date))
+            messagebox.showinfo(
+                title="Info", message="Row editted succesfully")
 
 
-def change_appearance_mode(task_treeview):
+def change_appearance_mode(task_treeview, notebook):
     if ctk.get_appearance_mode() == "Dark":
         ctk.set_appearance_mode("Light")
         task_treeview["style"] = "Light.Treeview"
+        notebook["style"] = "Light.TNotebook"
     else:
         ctk.set_appearance_mode("Dark")
         task_treeview["style"] = "Dark.Treeview"
- 
-#%% 4. Main
+        notebook["style"] = "Dark.TNotebook"
+
+
+def generate_gantt(tab2, task_treeview):
+    tasks = []
+    start_dates = []
+    end_dates = []
+
+    for item in task_treeview.get_children():
+        task, priority, sdate, sdate, edate = task_treeview.item(item)[
+            "values"]
+        start_date = pd.to_datetime(sdate)
+        end_date = pd.to_datetime(edate)
+        tasks.append(task)
+        start_dates.append(pd.to_datetime(start_date))
+        end_dates.append(pd.to_datetime(end_date))
+
+    start_dates = pd.to_datetime(start_dates)
+    end_dates = pd.to_datetime(end_dates)
+
+    fig, ax = plt.subplots(figsize=(5, 3))
+
+    ax.barh(tasks, (end_dates - start_dates), left=start_dates)
+
+    ax.xaxis.set_major_locator(mdates.DayLocator())
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    plt.xticks(rotation=45)
+
+    plt.title("Diagrama de Gantt")
+    plt.xlabel("Fecha")
+    plt.ylabel("Tareas")
+
+    canvas = FigureCanvasTkAgg(fig, master=tab2)
+    canvas.draw()
+    canvas.get_tk_widget().grid(row=1, column=0, sticky=tk.NSEW)
+
+
+# %% 4. Main
 if __name__ == "__main__":
     root = ctk.CTk()
     app = Gestor_nova.MainWindow(root)
